d329088f860738e835159c799511aa92d527e14b
sunxiaowei(孙小伟) <sunxiaowei@thinkenergy.net>
2022-03-18 14:35:20 +0800

    read readme file as a variable

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="flitton_fib_py",
     version="0.0.1",
     author="ohmycloud",
     author_email="ohmycloudy@gmail.com",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that calculates Fibonacci numbers",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ohmycloud/speed_up_your_python_with_rust",
     install_requires=[],
